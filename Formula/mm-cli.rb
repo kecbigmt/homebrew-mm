27771a99b263efad6ca8aaf91eb4edd3892809b4
kecy <keishiro.oyama@kecy.me>
2025-08-07 16:39:41 +0900

    fix: modify v0.1.0 formula

--- Formula/mm-cli.rb
+++ Formula/mm-cli.rb
@@ -1,7 +1,7 @@
-class Mmcli < Formula
+class MmCli < Formula
   desc "Personal Knowledge Management CLI Tool"
   homepage "https://github.com/kecbigmt/homebrew-mm"
-  url "https://github.com/kecbigmt/mm/releases/download/v0.1.0/mm-cli.tar.gz"
+  url "https://github.com/kecbigmt/homebrew-mm/releases/download/v0.1.0/mm-cli.tar.gz"
   sha256 "3d401b5f290c11fc67fe41f1e15c008e98d57937628bd316514db2199a519193"
   version "0.1.0"
 
--- Formula/mm-mcp.rb
+++ Formula/mm-mcp.rb
@@ -1,7 +1,7 @@
-class Mmcli < Formula
+class MmMcp < Formula
   desc "Personal Knowledge Management MCP Server"
   homepage "https://github.com/kecbigmt/homebrew-mm"
-  url "https://github.com/kecbigmt/mm/releases/download/v0.1.0/mm-mcp.tar.gz"
+  url "https://github.com/kecbigmt/homebrew-mm/releases/download/v0.1.0/mm-mcp.tar.gz"
   sha256 "49253fe4645282ea789f6b3214d87afb3b3f58a922a315fb3f514ffd7f0eeabd"
   version "0.1.0"
 
